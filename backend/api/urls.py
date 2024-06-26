1547837cac37bc538ff255378515ec274a87e36e
Abhishek Kumar <abhishek1py@gmail.com>
2024-06-26 08:29:32 +0530

    ModelMixin Started

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,6 @@ from api import views
 
 urlpatterns = [
     path('', views.home),
-    path('student-api/', views.StudentAPI.as_view()),
-    path('student-api/<int:id>', views.StudentAPI.as_view()),
+    # path('student-api/', views.StudentAPI.as_view()),
+    path('student-api/<int:pk>', views.StudentAPI.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from rest_framework.generics import ListAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+
 from .models import Student
 from .serializers import StudentSerializer
 
@@ -11,102 +14,45 @@ from .serializers import StudentSerializer
 def home(request):
     return render(request, 'index.html')
 
-    # ------------------------------------ VIEW ------------------------------
-class StudentAPI(APIView):
-    def get(self, request,id=None, format=None):
-        if id is not None:
-            try:
-                student = Student.objects.get(id=id)
-                serializer = StudentSerializer(student)
+class StudentAPI(ListAPIView, RetrieveModelMixin):
 
-                return Response(serializer.data)
-            except Student.DoesNotExist:
-                response = {'msg': 'Student not found..!'}
-                return Response(response, status=status.HTTP_404_NOT_FOUND)
+    """
+    In ListAPIView and CreateModelMixin not required PK.
+    RetrieveModelMixin and UpdateModelMixin and DestroyModelMixin required PK.
+    """
+    
+# # ------------------------------------ LIST VIEW ALL OBJ------------------------------
+    # queryset = Student.objects.all()
+    # serializer_class = StudentSerializer
+    # def get(self, request, *args, **kwargs):
+    #     return self.list(request, *args, **kwargs)
 
-        # If Id id None then send all data.
-        students = Student.objects.all()
-        serializer = StudentSerializer(students, many=True)
+# # ------------------------------------ RETRIVE SINGLE OBJ ONLY ------------------------------
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+# # ------------------------------------ CTEATE ------------------------------
+    # queryset = Student.objects.all()
+    # serializer_class = StudentSerializer
+    # def post(self, request, *args, **kwargs):
+    #     return self.create(request, *args, **kwargs)
 
-        # safe=False not required for single.
-        return Response(serializer.data)
-
-    # ------------------------------------ CTEATE ------------------------------
-    def post(self, request, format=None):
-        """
-        request.data = Returns parsed JSON data from the request body.
-        or
-        request.POST = Returns POST data from the request body.
-        
-        request.method = Returns the HTTP method of the request (e.g., 'GET', 'POST
-        request.query_params = Returns query parameters from the request URL.
-        request.GET --> request.query_params.get('key', default_value) [standard]
-        """
-        
-        serializer = StudentSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            response = {'msg': 'Data inserted successfully..!'}
-            return Response(response, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# -------------------------------- PUT -------------------------------------------------
-    def put(self, request, id, format=None):
-        if id is not None:
-            try:
-                student = Student.objects.get(id=id)
-                ''' partial=True : This will expect all fields to update otherwise you will
-                    get All fields required.'''
-              
-                serializer = StudentSerializer(student, data=request.data)
-
-                if serializer.is_valid():
-                    serializer.save()
-                    response = {'msg': 'Complete Data Updated Successfully..!'}
-                    return Response(response, status=status.HTTP_200_OK)
-
-                return Response(serializer.errors)
-
-            except Student.DoesNotExist:
-                return JsonResponse({"msg": "Student not found..!", 'status': 400})
-
-# -------------------------------- PATCH -------------------------------------------------
-    def patch(self, request, id, format=None):
-        if id is not None:
-            try:
-                student = Student.objects.get(id=id)
-
-                serializer = StudentSerializer(
-                    student, data=request.data, partial=True)  # partial update.
-
-                if serializer.is_valid():
-                    serializer.save()
-                    response = {'msg': 'Data Updated Successfully..!'}
-                    return Response(response, status=status.HTTP_200_OK)
-
-                return Response(serializer.errors)
-
-            except Student.DoesNotExist:
-                response = {"msg": "Student not found..!"}
-                return Response(response, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({'msg': 'ID Not Sent'}, status=status.HTTP_400_BAD_REQUEST)
-        
-
-# -------------------------------- DELETE ------------------------------------------
-    def delete(self, request, id, format=None):
-        if id is not None:
-            try:
-                student = Student.objects.get(id=id)
-                student.delete()
-                response = {'msg': 'Data Deleted Successfully..!'}
-                return Response(response, status=status.HTTP_200_OK)
-
-            except Student.DoesNotExist:
-                response = {"msg": "Student not found..!"}
-                return Response(response, status=status.HTTP_400_BAD_REQUEST)
-
-        response = {'msg': 'ID Not Sent'}
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+# # -------------------------------- PUT -------------------------------------------------
+    # queryset = Student.objects.all()
+    # serializer_class = StudentSerializer
+    # def put(self, request, *args, **kwargs):
+    #     return self.update(request, *args, **kwargs)
+    
+# # -------------------------------- PATCH -------------------------------------------------
+    # queryset = Student.objects.all()
+    # serializer_class = StudentSerializer
+    # def patch(self, request, *args, **kwargs):
+    #     return self.update(request, *args, **kwargs)
+    
+# # -------------------------------- DELETE ------------------------------------------
+    # queryset = Student.objects.all()
+    # serializer_class = StudentSerializer
+    # def delete(self, request, *args, **kwargs):
+    #     return self.destroy(request, *args, **kwargs)
